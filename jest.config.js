3b868a92064a4d2a298df852b37ab54ff7b57147
Joshua Hyde <jrh3k5@gmail.com>
2025-02-25 20:07:27 -0600

    Fix test config, build

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,24 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: "jsdom",
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
+  preset: "ts-jest",
   moduleNameMapper: {
     "^@/lib/(.*)$": "<rootDir>/src/lib/$1",
     "^@/models/(.*)$": "<rootDir>/src/models/$1",
     "^@/services/(.*)$": "<rootDir>/src/services/$1",
   },
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config);
