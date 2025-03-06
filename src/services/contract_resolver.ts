22c12c393ca9f7e71f9da1d57be07c7bbfe4cd28
Joshua Hyde <jrh3k5@gmail.com>
2025-03-05 22:12:17 -0600

    Update to v0.6.0 of smart contract (#82)

--- src/services/contract_resolver.ts
+++ src/services/contract_resolver.ts
@@ -9,7 +9,7 @@ export interface ContractResolver {
 // InMemoryContractResolver is an in-memory implementation of the ContractResolver interface.
 export class InMemoryContractResolver {
   private contractMappings: Map<string, string> = new Map([
-    [`${baseSepolia.id}`, "0x2008d605807B51928D045044810434764Df7991B"],
+    [`${baseSepolia.id}`, "0xDeEF024B1fFf995E572A08318B5f770b1402dB01"],
   ]);
 
   async resolveContractAddress(chainId: number): Promise<string> {
